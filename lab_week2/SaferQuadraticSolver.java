c40efb8c0276fb18194d8388484fe18932812b74
barcai <udvardy.zsombor@gmail.com>
2016-01-12 18:36:39 +0000

    Finished Week 2's Core exercises

--- lab_week2/SaferQuadraticSolver.java
+++ lab_week2/SaferQuadraticSolver.java
@@ -1,5 +1,17 @@
 public class SaferQuadraticSolver {
 	public static void main(String[] args) {
-		
+		Double a = Double.parseDouble(args[0]);
+		Double b = Double.parseDouble(args[1]);
+		Double c = Double.parseDouble(args[2]);
+		if ((Math.pow(b, 2) -4*a*c) < 0) {
+			System.out.println("Equation is not solvable for real x.");
+		}
+		else if (a == 0.0) {
+			System.out.println("A = 0. Cannot solve equation.");
+		}
+		else {
+			System.out.println(((-b + Math.sqrt((Math.pow(b, 2) - 4*a*c)))/2*a));
+			System.out.println(((-b - Math.sqrt((Math.pow(b, 2) - 4*a*c)))/2*a));
+		}
 	}
 }
---
+++ lab_week2/SquaresLoopRange.java
@@ -0,0 +1,15 @@
+public class SquaresLoopRange {
+	public static void main(String[] args) {
+		int start = Integer.parseInt(args[0]);
+		int stop = Integer.parseInt(args[1]) + 1;
+		if (start >= stop) {
+			System.out.println("Start-limit greater than stop-limit!");
+		}
+		else {
+			for (int i = start; i < stop; i ++) {
+				System.out.print((i*i) + " ");
+			}
+			System.out.println();
+		}
+	}
+}
