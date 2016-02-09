89e4c69abf579966097689f0a9749b228a23c92a
barcai <udvardy.zsombor@gmail.com>
2016-02-09 21:23:53 +0000

    Finished week7 q2

---
+++ LabSessions/src/week7/music/Favourites.java
@@ -0,0 +1,39 @@
+package week7.music;
+
+public class Favourites {
+	
+	private static final int numberOfTracks = 5;
+	
+	private int count = 0;
+	private MusicTrack[] favs = new MusicTrack[5];
+	
+	public void addTrack(String artist, String title) {
+		if (count < numberOfTracks) {
+			this.favs[count] = new MusicTrack(artist, title);
+			count++;
+		} else {
+			System.out.println("Sorry, can't add: " + new MusicTrack(artist, title));
+			System.out.println();
+		}
+	}
+	
+	public void showFavourites() {
+		for (MusicTrack track : favs) {
+			if (track != null) {
+				System.out.println(track);
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Favourites favourites = new Favourites();
+		favourites.addTrack("Fun", "Some Nights");
+		favourites.addTrack("Oliver Tank", "Help You Breathe");
+		favourites.addTrack("Horse Feathers", "Fit Against the Country");
+		favourites.addTrack("Emile Sande", "Country House");
+		favourites.addTrack("Fun", "Walking the Dog");
+		favourites.addTrack("Porcelain Raft", "Put Me To Sleep");
+		favourites.showFavourites();
+	}
+
+}
